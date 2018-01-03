bd6c08ad2130b65ab8c1a0e5d372b7b34a1a3859
Spotlight <awesomebing1@gmail.com>
2018-01-02 22:11:31 -0600

    Finally fix deployment syntax issues

--- .circleci/setup.sh
+++
@@ -1,6 +0,0 @@
-curl -sSL https://sdk.cloud.google.com > /tmp/gcl && bash /tmp/gcl --install-dir=$HOME --disable-prompts
-# Set up client secret for GAE later on.
-echo "$CLIENT_SECRET" | base64 --decode > ${HOME}/client-secret.json
-$HOME/google-cloud-sdk/bin/gcloud --quiet components update
-$HOME/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file $HOME/client-secret.json
-$HOME/google-cloud-sdk/bin/gcloud config set project $GCLOUD_PROJECT
