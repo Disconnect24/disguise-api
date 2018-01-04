6d666fa78d8469693f27c0b9d9719c8fb08c4377
Spotlight <awesomebing1@gmail.com>
2018-01-04 02:16:44 -0600

    Add authentication for delete
    also Mails to Mail for kind name. Embarassing typo :sweat_smile:

--- Delete.go
+++ Delete.go
@@ -16,42 +16,58 @@ func Delete(w http.ResponseWriter, r *http.Request) {
 	// Cloud Datastore seems to do this by default.
 	r.ParseForm()
 
-	wiiID := r.Form.Get("mlid")
-	if !FriendCodeIsValid(wiiID) {
-		fmt.Fprint(w, GenNormalErrorCode(610, "Invalid friend code."))
-		return
-	}
-
 	delnum, err := strconv.Atoi(r.Form.Get("delnum"))
 	if err != nil {
 		fmt.Fprint(w, GenNormalErrorCode(610, "delnum is invalid."))
 		return
 	}
-	query := datastore.NewQuery("Mails").
-		Filter("Delivered = ", true).
-		// Remove w from friend code
-		Filter("SenderID = ", wiiID[1:]).
-		Limit(delnum)
-	for mailToDelete := query.Run(ctx); ; {
-		var currentMail Mail
-		mailKey, err := mailToDelete.Next(&currentMail)
+
+	passwd := r.Form.Get("passwd")
+	if passwd == "" || len(passwd) != 16 {
+		fmt.Fprintf(w, GenNormalErrorCode(330, "Unable to parse parameters."))
+		return
+	}
+
+	// Check passwd
+	query := datastore.NewQuery("Accounts").Filter("Passwd = ", passwd).Limit(1)
+	for mlidResult := query.Run(ctx); ; {
+		var currentUser Accounts
+		mlidKey, err := mlidResult.Next(&currentUser)
 		if err == datastore.Done {
 			break
 		}
-		if err != nil {
-			log.Warningf(ctx, "Couldn't cycle through mail! %v", err)
-			fmt.Fprintf(w, GenNormalErrorCode(541, "Issue deleting mail from the database."))
-			return
+
+		query := datastore.NewQuery("Mail").
+			Filter("Delivered = ", true).
+		// Remove w from friend code
+			Filter("RecipientID = ", mlidKey.StringID()).
+			Limit(delnum)
+		for mailToDelete := query.Run(ctx); ; {
+			var currentMail Mail
+			mailKey, err := mailToDelete.Next(&currentMail)
+			if err == datastore.Done {
+				break
+			}
+			if err != nil {
+				log.Warningf(ctx, "Couldn't cycle through mail! %v", err)
+				fmt.Fprintf(w, GenNormalErrorCode(541, "Issue deleting mail from the database."))
+				return
+			}
+
+			// delet this
+			if datastore.Delete(ctx, mailKey) != nil {
+				log.Errorf(ctx, "Couldn't delete mail from database!")
+				fmt.Fprintf(w, GenNormalErrorCode(541, "Issue deleting mail from the database."))
+				return
+			}
 		}
 
-		// delet this
-		if datastore.Delete(ctx, mailKey) != nil {
-			log.Errorf(ctx, "Couldn't delete mail from database!")
-			fmt.Fprintf(w, GenNormalErrorCode(541, "Issue deleting mail from the database."))
-			return
-		}
+		fmt.Fprint(w, GenNormalErrorCode(100, "Success."),
+			"delnum=", delnum)
+		return
 	}
 
-	fmt.Fprint(w, GenNormalErrorCode(100, "Success."),
-		"delnum=", delnum)
+	// Only runs if not returned from earlier.
+	fmt.Fprintf(w, GenNormalErrorCode(220, "Invalid authentication."))
+	return
 }
