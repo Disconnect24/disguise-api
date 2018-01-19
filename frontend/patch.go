625701fc1740c68cd7c1398031abb82b5a9d37dc
Spotlight <spotlight@joscomputing.space>
2018-01-19 12:58:11 -0600

    Push unfinished patching logic

--- frontend/patch.go
+++ frontend/patch.go
@@ -1,5 +1,35 @@
 package frontend
 
+import (
+	"errors"
+	"encoding/binary"
+	"bytes"
+)
+
 func PatchNwcConfig(originalConfig []byte) (patchedConfig []byte, err error) {
-	return []byte("hi"), nil
+	if len(originalConfig) != 1024 {
+		return nil, errors.New("invalid config size")
+	}
+
+	var config ConfigFormat
+	configReadingBuf := bytes.NewBuffer(originalConfig)
+	err = binary.Read(configReadingBuf, binary.BigEndian, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	if bytes.Compare(config.Magic[:], ConfigMagic) != 0 {
+		return nil, errors.New("invalid magic")
+	}
+
+	copy(config.MailDomain[:], []byte("@mail.disconnect24.xyz"))
+
+	// The following is very redundantly written. TODO: fix that?
+	copy(config.AccountURL[:], []byte("http://mail.disconnect24.xyz/cgi-bin/account.cgi"))
+	copy(config.CheckURL[:], []byte("http://mail.disconnect24.xyz/cgi-bin/account.cgi"))
+	copy(config.ReceiveURL[:], []byte("http://mail.disconnect24.xyz/cgi-bin/account.cgi"))
+	copy(config.DeleteURL[:], []byte("http://mail.disconnect24.xyz/cgi-bin/account.cgi"))
+	copy(config.SendURL[:], []byte("http://mail.disconnect24.xyz/cgi-bin/account.cgi"))
+
+	return nil, nil
 }
--- frontend/structures.go
+++ frontend/structures.go
@@ -1,2 +1,22 @@
 package frontend
 
+var ConfigMagic = []byte("WcCf")
+
+type ConfigFormat struct {
+	Magic             [4]byte
+	Version           int32
+	FriendCode        int64
+	AmountOfCreations int32
+	HasRegistered     int32
+	MailDomain        [40]byte
+	Passwd            [20]byte
+	Mlchkid           [24]byte
+	AccountURL        [128]byte
+	CheckURL          [128]byte
+	ReceiveURL        [128]byte
+	DeleteURL         [128]byte
+	SendURL           [128]byte
+	_                 [220]byte // Most likely reserved.
+	TitleBooting      int32
+	Checksum          [4]byte
+}
