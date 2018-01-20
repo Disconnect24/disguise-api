7fbc8a462fa736e4a495161bd6214a82b704f99b
Spotlight <spotlight@joscomputing.space>
2018-01-20 11:47:27 -0600

    Properly add checksum

--- frontend/patch.go
+++ frontend/patch.go
@@ -5,16 +5,19 @@ import (
 	"encoding/binary"
 	"bytes"
 	"io/ioutil"
+	"log"
 )
 
-func PatchNwcConfig(originalConfig []byte) (patchedConfig []byte, err error) {
+// PatchNwcConfig takes an original config, applies needed patches to the URL and such,
+// updates the checksum and returns either nil, error or a patched config w/o error.
+func PatchNwcConfig(originalConfig []byte) ([]byte, error) {
 	if len(originalConfig) != 1024 {
 		return nil, errors.New("invalid config size")
 	}
 
 	var config ConfigFormat
 	configReadingBuf := bytes.NewBuffer(originalConfig)
-	err = binary.Read(configReadingBuf, binary.BigEndian, &config)
+	err := binary.Read(configReadingBuf, binary.BigEndian, &config)
 	if err != nil {
 		return nil, err
 	}
@@ -32,23 +35,35 @@ func PatchNwcConfig(originalConfig []byte) (patchedConfig []byte, err error) {
 	copy(config.DeleteURL[:128], []byte("http://mail.disconnect24.xyz/cgi-bin/delete.cgi"))
 	copy(config.SendURL[:128], []byte("http://mail.disconnect24.xyz/cgi-bin/send.cgi"))
 
-	// Checksum.
-	// We loop from 1020 to avoid current checksum.
-	// Take every 4 bytes, add 'er up!
-	for i := 0; i < 1020; i += 4 {
-
-	}
-
+	// Read from struct to buffer
 	fileBuf := new(bytes.Buffer)
 	err = binary.Write(fileBuf, binary.BigEndian, config)
 	if err != nil {
 		return nil, err
 	}
-
-	patchedConfig, err = ioutil.ReadAll(fileBuf)
+	patchedConfig, err := ioutil.ReadAll(fileBuf)
 	if err != nil {
 		return nil, err
-	} else {
-		return patchedConfig, nil
 	}
+
+	var checksumInt uint32
+
+	// Checksum.
+	// We loop from 1020 to avoid current checksum.
+	// Take every 4 bytes, add 'er up!
+	for i := 0; i < 1020; i += 4 {
+		log.Printf("hello, %s", i)
+		addition := binary.BigEndian.Uint32(patchedConfig[i:i+4])
+		checksumInt += addition
+	}
+
+	// Grab lower 32 bits of int
+	var finalChecksum uint32
+	finalChecksum = checksumInt & 0xFFFFFFFF
+	binaryChecksum := make([]byte, 4)
+	binary.BigEndian.PutUint32(binaryChecksum, finalChecksum)
+
+	// Update patched config checksum
+	copy(patchedConfig[1020:1024], binaryChecksum)
+	return patchedConfig, nil
 }
